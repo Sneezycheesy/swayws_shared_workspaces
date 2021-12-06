77991f43845ad1d631719a86229f75764345f181
Sneezycheesy <j-sjengske@hotmail.com>
2021-12-06 18:47:33 +0100

    Updated script to not use swayws module from Rust and (hopefully) fixed bug

--- wpo.sh
+++ wpo.sh
@@ -1,44 +1,22 @@
 #!/usr/bin/bash
-WORKSPACES_CURRENT=`swaymsg -t get_workspaces | jq '.[]|"focused=\(.focused) name=\(.name) output=\(.output) visible=\(.visible)"' | grep "focused=true"`
-OUTPUT_CURRENT=`awk -v var="$WORKSPACES_CURRENT" 'BEGIN{split(var, a);split(a[3], b, "=");print b[2];}'`
-WORKSPACE_CURRENT_NAME=`awk -v var="$WORKSPACES_CURRENT" 'BEGIN{split(var, a);split(a[2], b, "=");print b[2]}'`
+current_workspace=$(swaymsg -t get_workspaces | jq '.[]|"focused=\(.focused) name=\(.name) output=\(.output) visible=\(.visible)"' | grep "focused=true")
+current_output=$(echo "${current_workspace}" | awk '{split($0, a);split(a[3], b, "=");print b[2];}')
+current_name=$(echo "${current_workspace}" | awk '{split($0, a);split(a[2], b, "=");print b[2]}')
 
-WORKSPACES_NEW=`swaymsg -t get_workspaces | jq '.[]|"focused=\(.focused) name=\(.name) output=\(.output) visible=\(.visible)"' | grep "name=$1"`
+new_workspace=$(swaymsg -t get_workspaces | jq '.[]|"focused=\(.focused) name=\(.name) output=\(.output) visible=\(.visible)"' | grep "name=$1")
 #OUTPUT_NEW=`echo $WORKSPACES_NEW | awk '{print $3}' | awk -F'=' '{print $2}'`
-OUTPUT_NEW=`awk -v var="$WORKSPACES_NEW" 'BEGIN{split(var, a);split(a[3], output, "=");print output[2]}'`
+new_output=$(echo "${new_workspace}" | awk '{split($0, a);split(a[3], output, "=");print output[2]}')
 #VISIBLE_NEW=`echo $WORKSPACES_NEW | awk '{print $4}' | awk -F'=' '{print $2}' | awk -F'\"' '{print $1}'`
-VISIBLE_NEW=`awk -v var="$WORKSPACES_NEW" 'BEGIN{split(var, a);split(a[4], visible, "=");split(visible[2], visible, "\"");print visible[1]}'`
+new_visible=$(echo "${new_workspace}" | awk '{split($0, a);split(a[4], visible, "=");split(visible[2], visible, "\"");print visible[1]}')
 #FOCUSED_NEW=`echo $WORKSPACES_NEW | awk '{print $1}' | awk -F'=' '{print $2}'`
-FOCUSED_NEW=`awk -v var="$WORKSPACES_NEW" 'BEGIN{split(var, a);split(a[1], focused, "=");print focused[2]}'`
+new_focused=$(echo "${new_workspace}" | awk '{split($0, a);split(a[1], focused, "=");print focused[2]}')
 
+# If new desired workspace is already focused, just exit
+[ "$new_focused" = "false" ] || exit
 
-if [[ "$2" = "move" ]]; then
-    swaymsg move container workspace $1
-    if [[ -z $3 ]]; then 
-	exit
-    fi
-fi
+# If current output and new output are the same
+# Or new workspace does not exist
+# Just switch workspaces and exit
+[[ "${current_output}" = "${new_output}" || -n "${new_workspace}" ]] && swaymsg workspace $1 ; exit
 
-if [[ "$WORKSPACES_NEW" = "" ]]; then
-    swaymsg workspace $1
-else
-   if [[ "$FOCUSED_NEW" != "true" ]]; then
-        if [[ "$VISIBLE_NEW" != "true" ]]; then
-	    swayws move "$1" "$OUTPUT_CURRENT" && swayws focus $1
-        else
-            swayws move "$WORKSPACE_CURRENT_NAME" "$OUTPUT_NEW" ; swayws move "$1" "$OUTPUT_CURRENT" ; swayws focus $1
-	fi
-   fi
-fi
-
-if [[ "$FOCUSED_NEW" != "true"  ]]; then
-    if [[ "$VISIBLE_NEW" != "true" ]]; then
-	swayws move "$1" "$OUTPUT_CURRENT" && swayws focus $1
-    else
-	if [[ "" = "$WORKSPACES_NEW" ]]; then
-	    swaymsg workspace $1
-	else
-	    swayws move "$WORKSPACE_CURRENT_NAME" "$OUTPUT_NEW" ; swayws move "$1" "$OUTPUT_CURRENT" ; swayws focus $1
-	fi
-    fi
-fi
+swaymsg move workspace to output $new_output ; swaymsg workspace $1 ; swaymsg move workspace $current_output ; swaymsg workspace ${current_name} ; swaymsg workspace $1 ; exit
